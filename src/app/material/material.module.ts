7d47797169d461a2d64663bf14c25cb75af43c3a
DavidBlan7 <david1991blanco@gmail.com>
2021-03-18 14:13:53 +0100

    primero

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+
+
+  ]
+})
+export class MaterialModule { }
